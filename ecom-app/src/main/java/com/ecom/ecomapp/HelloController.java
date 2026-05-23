082975ec2e68526d439e9913e09093c16c2b8c21
Shivam kumar <sk8612181@gmail.com>
2026-05-23 23:55:41 +0530

    feat: update endpoint and greeting message in HelloController

--- ecom-app/src/main/java/com/ecom/ecomapp/HelloController.java
+++ ecom-app/src/main/java/com/ecom/ecomapp/HelloController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/home")
     public String hello() {
-        return "Hello, shivam!";
+        return "welcome home, shivam!";
     }
 
 }

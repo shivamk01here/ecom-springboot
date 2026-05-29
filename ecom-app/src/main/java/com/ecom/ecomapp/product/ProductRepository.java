6ecc3aefcd985fc6677de02a228a428ae2cc2aeb
Shivam kumar <sk8612181@gmail.com>
2026-05-29 22:24:40 +0530

    feat: add search query method in ProductRepository

--- ecom-app/src/main/java/com/ecom/ecomapp/product/ProductRepository.java
+++ ecom-app/src/main/java/com/ecom/ecomapp/product/ProductRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     List<ProductEntity> findByCategory(String category);
+    List<ProductEntity> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String name, String description);
 }
